1747914e76993266a44fa258f991066597856950
kayode adebayo <kaybabaguv20023@gmail.com>
2025-03-25 23:03:55 +0000

    deploy master update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 Developer at work
 Application is hereby ready to be deployed to the development environment
 Development must be reviewd by managers before deployment
-
+AWS Code deploy is a great too
